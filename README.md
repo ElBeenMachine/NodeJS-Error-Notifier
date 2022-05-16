0a11e0fa84f6aaccf1bb69526ce348f972520f31
Ollie <beenhamow@outlook.com>
2022-05-16 21:00:08 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # NodeJS-Error-Notifier
+
+This is a simple NodeJS module that can send developers an email if their application errors while unattended.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running.
+
+### Prerequisites
+
+What things you need to install the software and how to use them
+
+```
+- You wil need a Gmail account to send the emails from. 
+    - This email will need to have 2 factor authentication enabled.
+    - You will need to generate an application password for this email. See https://support.google.com/accounts/answer/185833?hl=en for more info.
+```
+
+### Installation
+
+Install the module with:
+
+```
+npm i --save nodejs-error-notifier
+```
+
+## Examples
+
+### Basic error handling
+
+The example below will send a notification to `example2@example.com` and `example3@example.com` on any unhandled exceptions.
+
+```js
+const ErrorNotifier = require("nodejs-error-notifier");
+const notifier = new ErrorNotifier({
+    senderEmail: "example@gmail.com",
+    senderPassword: "password123",
+    emails: ["example2@example.com", "example3@gmail.com"],
+    projectName: "My awesome project!"
+});
+
+process.on("unhandledException", (err) => {
+    notifier.process(err);
+});
+```
+
+## Other Information
+
+### Built With
+
+* [NodeJS](https://nodejs.org/)
+* [Nodemailer](https://nodemailer.com/) - For sending the notifications
+* [EJS](https://ejs.co/) - For rendering the email template
+
+
+### Versioning
+
+This project uses [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/ElBeenMachine/NodeJS-Error-Notifier/tags). 
+
+### Authors
+
+* **Ollie Beenham** - [Portfolio](http://beenhamow.xyz)
+
+See also the list of [contributors](https://github.com/ElBeenMachine/NodeJS-Error-Notifier/contributors) who participated in this project.
+
+### License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ index.js
@@ -0,0 +1,86 @@
+// Module imports
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+
+// Create notifier class
+
+module.exports = class Notifier {
+    // Set class properties
+    _projectName = "";
+    _emails = [];
+    _senderEmail = "";
+    _senderPassword = "";
+    _transporter;
+    _smtpConfig = {};
+
+    /**
+    * Create a notifier object
+    * @param {Object} options - Object with options
+    * @param {string} options.senderEmail - email address used to send notifications
+    * @param {string} options.senderPass - password from sender account
+    * @param {string|Array} options.emails - set of emails, which notifications are sent to (may be string or array of strings)
+    * @param {string} options.projectName - current project name
+    * @returns {Notifier}
+    */
+    constructor(options) {
+        // Parameter verification
+        if(!options.projectName) throw "Required field 'projectName' was not provided";
+        if(!options.emails) throw "Required field 'emails' was not provided";
+        if(!options.senderEmail) throw "Required field 'senderEmail' was not provided";
+        if(!options.senderPassword) throw "Required field 'senderPassword' was not provided";
+
+        if(!typeof options.projectName == "String") throw new TypeError("Field 'projectName' must be of type 'String'");
+        if(!typeof options.emails == "Array") throw new TypeError("Field 'emails' must be of type 'Array'");
+        if(!typeof options.senderEmail == "String") throw new TypeError("Field 'senderEmail' must be of type 'String'");
+        if(!typeof options.senderPassword == "String") throw new TypeError("Field 'senderPassword' must be of type 'String'");
+
+        // Set class properties
+        this._projectName = options.projectName;
+        this._emails = options.emails;
+        this._senderEmail = options.senderEmail;
+        this._senderPassword = options.senderPassword;
+
+        
+        this._smtpConfig = {
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: this._senderEmail,
+                pass: this._senderPassword
+            }
+        };
+
+        this._transporter = nodemailer.createTransport(this._smtpConfig);
+    }
+
+    /**
+    * Send notification
+    * @param {Error} error - Error Object
+    */
+    process(error) {
+        // If the parameter passed of not an Error object, throw a TypeError
+        if(!error instanceof Error) throw new TypeError("Required parameter 'error' must be an Error object.");
+
+        ejs.renderFile(__dirname + "/template.ejs", { projectName: this._projectName, error}, (err, data) => {
+            if(err) throw err;
+
+            // Set the mail options
+            var mailOptions = {
+                from: `"${this._projectName} Error Handler" ${this._senderEmail}`,
+                to: this._emails,
+                subject: `An error has occurred on your project: ${this._projectName}`,
+                html: data
+            };
+
+            this._transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(Object.getOwnPropertyNames(error));
+                    throw error;
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+        });
+    }
+}
